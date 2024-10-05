9e6a1c6b07bb9f08d65528cf5daf62765b762989
José Thevez Gomes Guedes <162639354+ThevezGomes@users.noreply.github.com>
2024-10-05 12:20:30 -0300

    Adicionei uma função de fitro
    Adicionei uma função que recebe um dataframe, uma coluna e alguns elementos, e retorna um novo dataframe apenas com as linhas que possuem aqueles elementos na coluna escolhida.

--- src/manipulacao_csv.py
+++ src/manipulacao_csv.py
@@ -46,12 +46,34 @@ def filtrar_colunas(dataframe: pd.core.frame.DataFrame, lista_colunas: list[str]
     novo_dataframe: pd.core.frame.DataFrame = dataframe[lista_colunas]
     return novo_dataframe
 
-def filtrar_linhas(dataframe, coluna_filtro, *args):
-    novo_dataframe = dataframe[dataframe[coluna_filtro] == args[0]]
+def filtrar_linhas(dataframe: pd.core.frame.DataFrame, coluna_filtro: str, *args) -> pd.core.frame.DataFrame:
+    """
+    Funca que recebe um dataframe, uma coluna especifica e elementos, e retorna um novo dataframe apenas com as linhas que possuem os elementos na coluna especifica.
+
+    Parameters
+    ----------
+    dataframe : pd.core.frame.DataFrame
+        Dataframe a ser filtrado.
+    coluna_filtro : str
+        Coluna especifica.
+    *args : TYPE
+        Elementos que serao procurados.
+
+    Returns
+    -------
+    novo_dataframe : TYPE
+        Dataframe filtrado apenas com as linhas que possuem os elementos na coluna especifica.
+
+    """
+    # Cria um novo dataframe que tem apenas as linhas que possuem o primeiro do elemento na coluna especifica
+    novo_dataframe:pd.core.frame.DataFrame = dataframe[dataframe[coluna_filtro] == args[0]]
+    # Se tiver mais de um elemento
     if len(args) > 1:
+        # Para cada elemento, pega o dataframe que tem apenas as linhas que possuem aquele elemento na coluna especifica
         for elemento_filtro in args[1:]:
-            dataframe_filtrado = dataframe[dataframe[coluna_filtro] == elemento_filtro]
+            dataframe_filtrado: pd.core.frame.DataFrame = dataframe[dataframe[coluna_filtro] == elemento_filtro]
+            # Funde esse dataframe com o dataframe a ser retornado
             novo_dataframe = pd.merge(novo_dataframe, dataframe_filtrado, how="outer")
         
+    # Retorna o dataframe filtrado
     return novo_dataframe
-        
