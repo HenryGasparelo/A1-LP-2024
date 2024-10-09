de661fb23c98bf08510495121773cb3f08549f56
José Thevez Gomes Guedes <162639354+ThevezGomes@users.noreply.github.com>
2024-10-09 18:29:11 -0300

    Remoção de acentos

--- src/preparacao_visualizacao.py
+++ src/preparacao_visualizacao.py
@@ -43,11 +43,11 @@ def modificar_dados_usando_dicionario(dataframe: pd.core.frame.DataFrame, coluna
         Dataframe modificado, onde cada elemento da coluna escolhida e o valor do dicionario do elemento original.
 
     """
-    # Cria uma cópia do dataframe original
+    # Cria uma copia do dataframe original
     dataframe_modificado: pd.core.frame.DataFrame = dataframe.copy()
-    # Aplica em cada elemento da coluna escolhida a função aplicar_dicionario, dicionario dentro de uma tupla para que não seja desempacotado
+    # Aplica em cada elemento da coluna escolhida a função aplicar_dicionario, dicionario dentro de uma tupla para que nao seja desempacotado
     dataframe_modificado[coluna] = dataframe_modificado[coluna].apply(aplicar_dicionario, args=(dicionario,))
-    # Retorna o dicionário modificado
+    # Retorna o dicionario modificado
     return dataframe_modificado
 
 def analise_unidimensional(dataframe: pd.core.frame.DataFrame, coluna: str) -> dict:
