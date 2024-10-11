b63301472e382184ec9f6c3ee63bca64198390d2
Davifvnunes <160730003+Davifvnunes@users.noreply.github.com>
2024-10-11 00:06:09 -0300

    Melhoramento de índice e espaçamento

--- plot/plot_hip4_1.py
+++ plot/plot_hip4_1.py
@@ -1,4 +1,4 @@
-#Hipotese 4.1
+#Hipotese 4.1 linhas empilhadas com marcadores
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -11,19 +11,27 @@ sys.path.append('../src')
 import dataframes_hipoteses as dh
 import main as mn
 
-#tema de fundo
 sns.set_theme(style="whitegrid")
 
-#dataframe
+# Carregar o dataframe
 df = pd.DataFrame(dh.gerar_dataframe_hipotese1_original(mn.dados))
 
-#paleta
+# Paleta personalizada
 paleta_linda_hex = ['#DCD301', '#DC0701', '#001EDB']
 
-#grafo de linhas  empilhadas com marcadores
-sns.lineplot(
+# Criar o gráfico de linhas empilhadas com marcadores
+d = sns.lineplot(
     data=df,
     x="AnosCodando", y="SalarioMedio", hue="Satisfacao", style="Satisfacao",
-    markers=True, dashes=False, palette= paleta_linda_hex
+    markers=True, dashes=False, palette=paleta_linda_hex
 )
+
+# Ajustar os rótulos dos eixos
+plt.xlabel("Anos Codando", labelpad=17)
+plt.ylabel("Salário Médio", labelpad=17)
+
+# Personalizar o título da legenda
+d.legend(title="Nível de Satisfação", title_fontsize='13', loc='best')
+
+# Exibir o gráfico
 plt.show()
