339e323f3f39dead19f324d7a34e5fc0f0819b02
José Thevez Gomes Guedes <162639354+ThevezGomes@users.noreply.github.com>
2024-10-11 19:33:19 -0300

    Adicionei docstring e documentação extra

--- plot/plot_geral_hipoteses.py
+++ plot/plot_geral_hipoteses.py
@@ -1,9 +1,11 @@
+"""MODULO PARA PLOTAGEM DE TODOS OS GRAFICOS"""
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import sys
 
-# Para poder importar o módulo a partir do diretório '../src'
+# Para poder importar o modulo a partir do diretorio '../src'
 sys.path.append('../src')
 
 import manipulacao_csv as mc
@@ -44,14 +46,27 @@ lista_colunas: list[str] = ["Student",
 # Dataframe apenas com as colunas selecionadas
 dados_filtrados: pd.core.frame.DataFrame = mc.filtrar_colunas(dados, lista_colunas)
 
-def plotar_h1(dataframe):
+def plotar_h1(dataframe: pd.core.frame.DataFrame):
+    """
+    Funcao que plota o grafico da hipotese 1.
+
+    Parameters
+    ----------
+    dataframe : pd.core.frame.DataFrame
+        Dataframe principal.
+
+    Returns
+    -------
+    None.
+
+    """
     # Estilo do fundo 
     sns.set_theme(style="whitegrid")
 
-    # Gerar DataFrame usando a função personalizada
+    # Gerar DataFrame usando a funcao personalizada
     df = pd.DataFrame(dh.gerar_dataframe_hipotese1(dados))
 
-    # Usar pivot_table para agregar valores duplicados (por exemplo, usando a média)
+    # Usar pivot_table para agregar valores duplicados (por exemplo, usando a media)
     df_p = df.pivot_table(index="AnosCodando", columns="LinguagemProgramacao", values="HabitosSaudaveis", aggfunc="mean")
 
     # Inverter os anos
@@ -61,56 +76,76 @@ def plotar_h1(dataframe):
     f, ax = plt.subplots(figsize=(9, 6))
     sns.heatmap(df_p, annot=True, fmt=".2f", cmap="magma", ax=ax)
 
-    # Definir os rótulos dos eixos
+    # Definir os rotulos dos eixos
     plt.xlabel("Linguagens Usadas", labelpad=17)
     plt.ylabel("Anos Codando", labelpad=17)
 
-    # Adicionar uma caixa de texto explicando o índice
+    # Adicionar uma caixa de texto explicando o indice
     plt.text(1.0, 1.05, "Índice de Hábitos Saudáveis",
             transform=plt.gca().transAxes, fontsize=12, bbox=dict(facecolor='white', alpha=0.6))
 
 
 
-    # Mostrar o gráfico
+    # Mostrar o grafico
     plt.tight_layout()
     plt.show()
 
-#Teste 1
-#plotar_h1(dados)
 
+def plotar_h2(dataframe: pd.core.frame.DataFrame):
+    """
+    Funcao que plota o grafico da hipotese 2.
 
-def plotar_h2(dataframe):
-    #Estilo de Fundo
+    Parameters
+    ----------
+    dataframe : pd.core.frame.DataFrame
+        Dataframe principal.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Estilo de Fundo
     sns.set_theme(style="whitegrid")
 
     # Carregar o dataset
     df = pd.DataFrame(dh.gerar_dataframe_hipotese2(dados))
 
-    # Criar o gráfico
+    # Criar o grafico
     g = sns.lmplot(
         data=df,
         x="PeriodoCodando", y="Salario", hue="LinguagemProgramacao",
         height=8, palette="tab10"
     )
 
-    # Alterar o título da legenda (índice do hue)
-    g._legend.set_title("Linguagens", prop={'size': 15})  # Tamanho do título
+    # Alterar o titulo da legenda (indice do hue)
+    g._legend.set_title("Linguagens", prop={'size': 15})  # Tamanho do titulo
 
-    #plt.legend(title="Linguagens", title_fontsize=15, fontsize=13)
+    # plt.legend(title="Linguagens", title_fontsize=15, fontsize=13)
 
-    # Ajustar os rótulos dos eixos
+    # Ajustar os rotulos dos eixos
     plt.xlabel("Período Codando (Anos)", labelpad=20)
     plt.ylabel("Salário (USD por Ano)", labelpad=20)
 
-    # Exibir o gráfico
+    # Exibir o grafico
     plt.show()
 
-#teste 2
-#plotar_h2(dados)
 
+def plotar_h3(dataframe: pd.core.frame.DataFrame):
+    """
+    Funcao que plota o grafico da hipotese 3.
 
-def plotar_h3(dataframe):
-    # Definir o tema e estilo do gráfico
+    Parameters
+    ----------
+    dataframe : pd.core.frame.DataFrame
+        Dataframe principal.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Definir o tema e estilo do grafico
     sns.set_theme(style="whitegrid")
 
     # Criar um DataFrame a partir dos dados
@@ -119,50 +154,57 @@ def plotar_h3(dataframe):
     # Definir uma paleta personalizada
     paleta_linda_hex = ['#DCD301', '#DC0701', '#001EDB', '#C800DB', '#00DBA3', '#BD7200']
 
-    # Criar o gráfico de linhas
+    # Criar o grafico de linhas
     plt.figure(figsize=(10, 6))  # Definir o tamanho da figura
     sns.lineplot(data=df, x="AnosCodando", y="Empregabilidade", hue="NiveisEducacionais", palette=paleta_linda_hex)
 
 
-    # Adicionar títulos e rótulos com espaçamento personalizado
-    plt.xlabel("Anos Codando", labelpad=15)  # Espaçamento do rótulo do eixo X
-    plt.ylabel("Empregabilidade", labelpad=15)  # Espaçamento do rótulo do eixo Y
+    # Adicionar titulos e rotulos com espacamento personalizado
+    plt.xlabel("Anos Codando", labelpad=15)  # Espacamento do rotulo do eixo X
+    plt.ylabel("Empregabilidade", labelpad=15)  # Espacamento do rotulo do eixo Y
 
-    # Ajustar o tamanho e a posição da legenda (hue)
+    # Ajustar o tamanho e a posicao da legenda (hue)
     plt.legend(title="Níveis Educacionais", title_fontsize=14, fontsize=12, loc="lower center")
 
-    # Mostrar o gráfico
+    # Mostrar o grafico
     plt.show()
 
-#Teste
-#plotar_h3(dados)
 
-def plotar_h4(dataframe):
+def plotar_h4(dataframe: pd.core.frame.DataFrame):
+    """
+    Funcao que plota o grafico da hipotese 4.
+
+    Parameters
+    ----------
+    dataframe : pd.core.frame.DataFrame
+        Dataframe principal.
+
+    Returns
+    -------
+    None.
+
+    """
     sns.set_theme(style="whitegrid")
 
     # Carregar o dataframe
-    df = pd.DataFrame(dh.gerar_dataframe_hipotese1_original(dados))
+    df = pd.DataFrame(dh.gerar_dataframe_hipotese4(dados))
 
     # Paleta personalizada
     paleta_linda_hex = ['#DCD301', '#DC0701', '#001EDB']
 
-    # Criar o gráfico de linhas empilhadas com marcadores
+    # Criar o grafico de linhas empilhadas com marcadores
     d = sns.lineplot(
         data=df,
         x="AnosCodando", y="SalarioMedio", hue="Satisfacao", style="Satisfacao",
         markers=True, dashes=False, palette=paleta_linda_hex
     )
 
-    # Ajustar os rótulos dos eixos
+    # Ajustar os rotulos dos eixos
     plt.xlabel("Anos Codando", labelpad=17)
     plt.ylabel("Salário Médio Anual", labelpad=17)
 
-    # Personalizar o título da legenda
+    # Personalizar o titulo da legenda
     d.legend(title="Nível de Satisfação", title_fontsize='13', loc='best')
 
-    # Exibir o gráfico
+    # Exibir o grafico
     plt.show()
-
-
-#teste
-#plotar_h4(dados)
